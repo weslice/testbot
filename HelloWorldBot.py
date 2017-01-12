e3c283740eb0d0cbb4acdcc1d98d9773193b65eb
weslice@hotmail.com <weslice@hotmail.com>
2017-01-12 19:59:37 -0200

    nova atualizacao

--- HelloWorldBot.py
+++ HelloWorldBot.py
@@ -3,10 +3,12 @@ import requests
 import json
 import traceback
 import random
+import json 
 app = Flask(__name__)
 
 token = "EAAa8E6VNt3EBADvH7cWPONPG8q10a2vkBDCDZAT8t3iml7TvGncZBZBljVZCZAH1tFGXI0DqXZB4CPAFE2NCm3qy1PCs3hB09OSfsRZCe0nv3Y9S9szKe0k9MTw6K6Owb6WQdA79gbJLk1nisUdCQHZCWnWxEb11TaETbr8T2ZB1DNgZDZD"
 tas = requests.get('https://graph.facebook.com/v2.6/me?access_token=' + token)
+das = tas.name
       
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
@@ -15,7 +17,7 @@ def webhook():
       data = json.loads(request.data)
       text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
       sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-      payload = {'recipient': {'id': sender}, 'message': {'text': "Hello Wes" }} # We're going to send this back
+      payload = {'recipient': {'id': sender}, 'message': {'text': "aaaa"}} # We're going to send this back
       r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
     except Exception as e:
       print traceback.format_exc() # something went wrong
