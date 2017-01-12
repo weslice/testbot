d701ff853b3e32cee8f722557a4f692cc1b27d16
weslice@hotmail.com <weslice@hotmail.com>
2017-01-12 19:44:44 -0200

    nova atualizacao

--- HelloWorldBot.py
+++ HelloWorldBot.py
@@ -15,7 +15,7 @@ def webhook():
       data = json.loads(request.data)
       text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
       sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-      payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}} # We're going to send this back
+      payload = {'recipient': {'id': sender}, 'message': {'text': tas.name}} # We're going to send this back
       r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
     except Exception as e:
       print traceback.format_exc() # something went wrong
