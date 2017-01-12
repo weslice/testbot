f53ad8b807432405b1b994a8cedb073b1c234e08
weslice@hotmail.com <weslice@hotmail.com>
2017-01-12 19:27:00 -0200

    aa

--- HelloWorldBot.py
+++ HelloWorldBot.py
@@ -11,11 +11,10 @@ token = "EAAa8E6VNt3EBADvH7cWPONPG8q10a2vkBDCDZAT8t3iml7TvGncZBZBljVZCZAH1tFGXI0
 def webhook():
   if request.method == 'POST':
     try:
-	  
-	  data = json.loads(request.data)
+      data = json.loads(request.data)
       text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
       sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-      payload = {'recipient': {'id': sender}, 'message': {'text': "Ola "}} # We're going to send this back
+      payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}} # We're going to send this back
       r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
     except Exception as e:
       print traceback.format_exc() # something went wrong
